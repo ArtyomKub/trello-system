9397a1c28c007dd25dd0f42f3d88edc979105b9d
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-17 22:42:49 +0200

    Add title in Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import {TodoList} from './TodoList';
 
+
+
+
 function App() {
+
+    const todoListTitle_1 = 'What to learn'
+    const todoListTitle_2 = 'What to buy'
+
     return (
         <div className="App">
-            <TodoList/>
-            <TodoList/>
-            <TodoList/>
+            <TodoList title={todoListTitle_1} />
+            <TodoList title={todoListTitle_2} />
         </div>
     );
 }
