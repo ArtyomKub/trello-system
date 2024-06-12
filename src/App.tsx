8ad5201067b2dbf0f9617a9d10b157e1d948a601
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-06-12 11:03:28 +0200

    Delete unused object tasks

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,21 @@ import {TaskType, TodoList} from './TodoList';
 
 function App() {
 
-    const todoListTitle_1 = 'What to learn'
-    // const todoListTitle_1 = 'What to learn'
-    const todoListTitle_2 = 'What to buy'
-    const tasks_1: Array<TaskType> = [
+    const todoListTitle = 'What to learn'
+    const tasks: Array<TaskType> = [
         {id: 1, isDone: true, title: 'HTML&CSS'},
         {id: 2, isDone: false, title: 'JS'},
         {id: 3, isDone: true, title: 'REACT'},
         {id: 4, isDone: false, title: 'CSS'},
     ]
 
-    const tasks_2: Array<TaskType> = [
-        {id: 1, isDone: false, title: 'Coffee'},
-        {id: 2, isDone: true, title: 'Juice'},
-        {id: 3, isDone: false, title: 'Meat'},
-        {id: 4, isDone: true, title: 'Chease'},
-    ]
 
     return (
         <div className="App">
-            <TodoList title={todoListTitle_1} tasks={tasks_1}/>
-            <TodoList title={todoListTitle_2} tasks={tasks_2}/>
+            <TodoList
+                title={todoListTitle}
+                tasks={tasks}
+            />
         </div>
     );
 }
